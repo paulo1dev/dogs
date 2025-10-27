99823fca8e77de2da5abb37c467633df0748a79a
Paulo <paulodev2003@gmail.com>
2025-10-26 23:54:48 -0300

    base da aplicacao criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Api from "./api/Api";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes >
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
